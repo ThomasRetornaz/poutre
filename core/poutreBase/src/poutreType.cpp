3242eb17c4bff88aa87e2aa4701c3005e7150d6c
thomas@thomas-mongex64 <thomas@thomas-mongex64>
2015-05-10 21:09:41 +0200

    Add CompOpType,ArithOpType,AssignOpType enum with serialisation cpp and todo tests

---
+++ core/poutreBase/src/poutreType.cpp
@@ -0,0 +1,191 @@
+#include <poutreBase/poutreTypes.hpp>
+
+namespace poutre
+  {
+  //NOTE JUST FOR CONVENIENCE AT INTERFACE LAYER TO USE STRING INSTEAD OF VERBOSE ENUM
+
+  std::ostream& operator<<(std::ostream& os, CompOpType arithOpType)
+    {
+    switch (arithOpType)
+      {
+        case CompOpType::CompOpDiff:
+          os << "!=";
+          break;
+        case CompOpType::CompOpEqual:
+          os << "==";
+          break;
+        case CompOpType::CompOpInf:
+          os << "<";
+          break;
+        case CompOpType::CompOpInfEqual:
+          os << "<=";
+          break;
+        case CompOpType::CompOpSup:
+          os << ">";
+          break;
+        case CompOpType::CompOpSupEqual:
+          os << ">=";
+          break;
+        default://  CompOpType::CompOpUndef; 
+          os << "Unknown CompOpType";
+          break;
+      }
+    return os;
+    }
+
+
+  std::istream& operator>>(std::istream& is, CompOpType& arithOpType)
+    {
+    arithOpType = CompOpType::CompOpUndef;
+
+    if (!is.good( ))
+      return is;
+
+    std::string strType;
+    is >> strType;
+    if (is.bad( ))
+      return is;
+
+    if (strType == "!=")
+      arithOpType = CompOpType::CompOpDiff;
+    else if (strType == "==")
+      arithOpType = CompOpType::CompOpEqual;
+    else if (strType == "<")
+      arithOpType = CompOpType::CompOpInf;
+    else if (strType == ">")
+      arithOpType = CompOpType::CompOpSup;
+    else if (strType == "<=")
+      arithOpType = CompOpType::CompOpInfEqual;
+    else if (strType == ">=")
+      arithOpType = CompOpType::CompOpSupEqual;
+    else
+      {
+      POUTRE_RUNTIME_ERROR("Unable to read arithOpType from stream");
+      }
+    return is;
+    }
+
+
+ std::ostream& operator<<(std::ostream& os, ArithOpType arithOpType)
+    {
+    switch (arithOpType)
+      {
+        case ArithOpType::ArithOpPlus:
+          os << "+";
+          break;
+        case ArithOpType::ArithOpMinus:
+          os << "-";
+          break;
+        case ArithOpType::ArithOpMul:
+          os << "*";
+          break;
+        case ArithOpType::ArithOpDiv:
+          os << "/";
+          break;
+        case ArithOpType::ArithOpMod:
+          os << "%";
+          break;
+        default://  ArithOpType::; 
+          os << "Unknown ArithOpType";
+          break;
+      }
+    return os;
+    }
+
+
+  std::istream& operator>>(std::istream& is, ArithOpType& arithOpType)
+    {
+    arithOpType = ArithOpType::ArithOpUndef;
+
+    if (!is.good( ))
+      return is;
+
+    std::string strType;
+    is >> strType;
+    if (is.bad( ))
+      return is;
+
+    if (strType == "+")
+      arithOpType = ArithOpType::ArithOpPlus;
+    else if (strType == "-")
+      arithOpType = ArithOpType::ArithOpMinus;
+    else if (strType == "/")
+      arithOpType = ArithOpType::ArithOpDiv;
+    else if (strType == "%")
+      arithOpType = ArithOpType::ArithOpMod;
+    else if (strType == "*")
+      arithOpType = ArithOpType::ArithOpMul;
+    else
+      {
+      POUTRE_RUNTIME_ERROR("Unable to read ArithOpType from stream");
+      }
+    return is;
+    }
+
+
+
+  std::ostream& operator<<(std::ostream& os, AssignOpType assignOpType)
+    {
+    switch (assignOpType)
+      {
+        case AssignOpType::AssignOpAdd:
+          os << "+=";
+          break;
+        case AssignOpType::AssignOpMinus:
+          os << "-=";
+          break;
+        case AssignOpType::AssignOpMul:
+          os << "*=";
+          break;
+        case AssignOpType::AssignOpDiv:
+          os << "/=";
+          break;
+        case AssignOpType::AssignOpMod:
+          os << "%=";
+          break;
+        case AssignOpType::AssignOp:
+          os << "=";
+          break;
+        default://  AssignOpType::; 
+          os << "Unknown AssignOpType";
+          break;
+      }
+    return os;
+    }
+
+
+  std::istream& operator>>(std::istream& is, AssignOpType& assignOpType)
+    {
+    assignOpType = AssignOpType::AssignOpUndef;
+
+    if (!is.good( ))
+      return is;
+
+    std::string strType;
+    is >> strType;
+    if (is.bad( ))
+      return is;
+
+    if (strType == "+=")
+      assignOpType = AssignOpType::AssignOpAdd;
+    else if (strType == "-=")
+      assignOpType = AssignOpType::AssignOpMinus;
+    else if (strType == "*=")
+      assignOpType = AssignOpType::AssignOpMul;
+    else if (strType == "/=")
+      assignOpType = AssignOpType::AssignOpDiv;
+    else if (strType == "%=")
+      assignOpType = AssignOpType::AssignOpMod;
+    else if (strType == "=")
+      assignOpType = AssignOpType::AssignOp;
+    else
+      {
+      POUTRE_RUNTIME_ERROR("Unable to read AssignOpType from stream");
+      }
+    return is;
+    }
+
+  
+  
+
+  }
---
+++ coreTests/poutreBase/types.cpp
@@ -0,0 +1,10 @@
+#include "main.hpp"
+#include <iostream>
+#include <thread>
+#include <poutreBase/poutreTypes.hpp>
+
+BOOST_AUTO_TEST_SUITE(typebase)
+
+
+
+BOOST_AUTO_TEST_SUITE_END( )
