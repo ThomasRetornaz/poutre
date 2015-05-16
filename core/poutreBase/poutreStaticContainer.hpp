e6b76c3428ea07f6c514d0bd30c8704dd2a6637b
thomas@thomas-mongex64 <thomas@thomas-mongex64>
2015-05-16 16:25:35 +0200

    ras

---
+++ core/poutreBase/poutreStaticContainer.hpp
@@ -0,0 +1,18 @@
+#ifndef POUTRE_STATIC_CONTAINER_HPP__
+#define POUTRE_STATIC_CONTAINER_HPP__
+
+/**
+ * @file   poutreStaticContainer.hpp
+ * @author Thomas Retornaz
+ * @brief  
+ * 
+ * 
+ */
+
+namespace poutre
+{
+
+}
+
+
+#endif //POUTRE_STATIC_CONTAINER_HPP__
