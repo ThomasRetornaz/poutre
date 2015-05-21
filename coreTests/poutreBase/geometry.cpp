b9d261dee339671cd177657ea80493901b12d1dc
thomas@thomas-mongex64 <thomas@thomas-mongex64>
2015-05-21 16:31:46 +0200

    ras

---
+++ coreTests/poutreBase/geometry.cpp
@@ -0,0 +1,36 @@
+
+//==============================================================================
+//                  Copyright (c) 2015 - Thomas Retornaz                      //
+//                     thomas.retornaz@mines-paris.org                        //
+//          Distributed under the Boost Software License, Version 1.0.        //
+//                 See accompanying file LICENSE.txt or copy at               //
+//                     http://www.boost.org/LICENSE_1_0.txt                   //
+//==============================================================================
+
+#include "main.hpp"
+#include <iostream>
+#include <thread>
+#include <poutreBase/poutreGeometry.hpp>
+
+BOOST_AUTO_TEST_SUITE(geometrybase)
+
+BOOST_AUTO_TEST_CASE(ctor)
+  {
+  using namespace poutre;
+  pt1D_scoord pt1;
+  pt2D_scoord pt2;
+  pt3D_scoord pt3;
+  pt4D_scoord pt4;
+
+  bb1D_scoord bb1;
+  bb2D_scoord bb2;
+  bb3D_scoord bb3;
+  bb4D_scoord bb4;
+
+  pt1D_rcoord ptr1;
+  pt2D_rcoord ptr2;
+  pt3D_rcoord ptr3;
+  pt4D_rcoord ptr4;
+  }
+
+BOOST_AUTO_TEST_SUITE_END( )
