a1a34c975b776fb5a742974ab00923ed1a36b704
thomas@thomas-mongex64 <thomas@thomas-mongex64>
2016-08-15 14:15:03 +0200

    Add benchmark as third party library

---
+++ bench/poutreBase/main.hpp
@@ -0,0 +1,16 @@
+
+//==============================================================================
+//                  Copyright (c) 2015 - Thomas Retornaz                      //
+//                     thomas.retornaz@mines-paris.org                        //
+//          Distributed under the Boost Software License, Version 1.0.        //
+//                 See accompanying file LICENSE.txt or copy at               //
+//                     http://www.boost.org/LICENSE_1_0.txt                   //
+//==============================================================================
+
+#ifndef _COREBENCHS_MAIN_HPP__
+#define _COREBENCHS_MAIN_HPP__
+
+#include <iostream>
+
+#endif //_COREBENCHS_MAIN_HPP__
+
