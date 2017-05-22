30c84b824157702b22356de0a545aa61c20e4521
thomas@thomas-mongex64 <thomas@thomas-mongex64>
2017-05-22 18:01:23 +0200

    chrono split hpp /cpp

--- src/poutreBase/poutreChronos.hpp
+++ src/poutreBase/poutreChronos.hpp
@@ -39,7 +39,7 @@ namespace poutre
       * start,stop interface and serialisation capabilites
       * @todo think about multi-threading
       */
-    class /*BASE_API*/ Timer //see http://en.cppreference.com/w/cpp/chrono/c/clock
+    class BASE_API Timer //see http://en.cppreference.com/w/cpp/chrono/c/clock
     {
     public:
         using  high_resolution_clock = std::chrono::high_resolution_clock;
@@ -47,118 +47,64 @@ namespace poutre
         using  timerep = double_milliseconds::rep; //todo use
                                                   //decltype(auto)
         //!ctor 
-        Timer(void) :m_start(), m_accu(), m_start_cputime(0), m_accu_cputime(0), m_nbiter(0)
-        {
-        }
+		Timer(void);
 
         //!dor 
-        ~Timer(void) POUTRE_NOEXCEPT
-        {
-        }
+		~Timer(void) POUTRE_NOEXCEPT;
 
         //!Start the timing action
-        void Start() POUTRE_NOEXCEPT
-        {
-            m_nbiter++;
-            m_start = high_resolution_clock::now();
-            m_start_cputime = std::clock();
-        }
-        //!Stop the timing action
-        void Stop() POUTRE_NOEXCEPT
-        {
-            m_accu += std::chrono::duration_cast<double_milliseconds>(high_resolution_clock::now() - m_start).count();
-            m_accu_cputime += std::clock() - m_start_cputime;
-        }
+		void Start() POUTRE_NOEXCEPT;
+
+		//!Stop the timing action
+		void Stop() POUTRE_NOEXCEPT;
 
         //!Grab wall time accumulated in ms
-        const timerep GetCumulativeTime() const POUTRE_NOEXCEPT
-        {
-            return m_accu;
-        }
+		const timerep GetCumulativeTime() const POUTRE_NOEXCEPT;
 
-        //!Grab wall mean time of iteration in ms
-        const timerep GetMeanTime() const POUTRE_NOEXCEPT
-        {
-            if (m_nbiter == 0) { return static_cast<timerep>(0); }
-            return GetCumulativeTime() / static_cast<timerep>(m_nbiter);
-        }
+		//!Grab wall mean time of iteration in ms
+		const timerep GetMeanTime() const POUTRE_NOEXCEPT;
 
         //!Grab cpu time accumulated in ms
-        const timerep GetCumulativeCPUTime() const POUTRE_NOEXCEPT
-        {
-            return static_cast<timerep>(m_accu_cputime/ CLOCKS_PER_SEC)*1000;
-        }
+		const timerep GetCumulativeCPUTime() const POUTRE_NOEXCEPT;
 
         //!Grab wall mean time of iteration in ms
-        const timerep GetMeanCpuTime() const POUTRE_NOEXCEPT
-        {
-            if (m_nbiter == 0) { return static_cast<timerep>(0); }
-            return static_cast<timerep>(GetCumulativeCPUTime()) / static_cast<timerep>(m_nbiter);
-        }
+		const timerep GetMeanCpuTime() const POUTRE_NOEXCEPT;
 
         //!Grab number off triggered start
-        const std::size_t NbIter() const POUTRE_NOEXCEPT
-        {
-            return m_nbiter;
-        }
-
+		const std::size_t NbIter() const POUTRE_NOEXCEPT;
+        
         //!String serialization
-        const std::string to_str() const POUTRE_NOEXCEPT
-        {
-            std::stringstream sstr;
-            sstr << "Cumulative wall time  in ms: " << GetCumulativeTime() << "\n Nb iter " << m_nbiter << "\n Mean wall time in ms" << GetMeanTime();
-            sstr << "Cumulative cpu time  in ms: " << GetCumulativeCPUTime() << "\n Nb iter " << m_nbiter << "\n Mean cpu time in ms" << GetMeanCpuTime();
-            return sstr.str();
-        }
+		const std::string to_str() const POUTRE_NOEXCEPT;
 
         //!Reset the chrono
-        void Reset()
-        {
-            m_accu = 0.;
-            m_start_cputime = 0;
-            m_accu_cputime = 0;
-            m_nbiter = 0;
-        }
+		void Reset() POUTRE_NOEXCEPT;
 
     private:
-        friend std::ostream & operator<<(std::ostream &os, Timer& timer);
         std::chrono::high_resolution_clock::time_point m_start; //!start wall time
         timerep m_accu; //! accumulate wall time over all iteration 
         std::clock_t m_start_cputime; //!start cpu time
         std::clock_t m_accu_cputime; //! accumulate cpu time
         std::size_t m_nbiter; //! nb iteration
-
-
     };
 
     //!Timer stream serialization
-    std::ostream & operator<<(std::ostream &os, Timer& timer)
-    {
-        os << timer.to_str();
-        return os;
-    }
+	BASE_API std::ostream& operator<<(std::ostream &os, Timer& timer);
 
     /**
      * @brief Scoped (RAII) timer, start and stop of embedded timer are automatically triggered
      *
      * @todo think about multi-threading
      */
-    class /*BASE_API*/ ScopedTimer
+    class BASE_API ScopedTimer
     {
     private:
         Timer& m_timer; //!Inner timer
 
     public:
         //!ctor
-        ScopedTimer(Timer& itimer) :m_timer(itimer)
-        {
-            m_timer.Start();
-        }
+		ScopedTimer(Timer& itimer);
         //!dtor
-        ~ScopedTimer(void)
-        {
-            m_timer.Stop();
-        }
+		~ScopedTimer(void) POUTRE_NOEXCEPT;
     };
 }
 
