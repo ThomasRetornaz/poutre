557a054ee09df2dec874fc3f0a1809f7d2560b18
thomas@thomas-mongex64 <thomas@thomas-mongex64>
2017-05-22 18:03:46 +0200

    boost simd change

--- nrt/poutreBase/boostSimdCleanImport.cpp
+++ nrt/poutreBase/boostSimdCleanImport.cpp
@@ -8,26 +8,25 @@
 //==============================================================================
 
 #include "main.hpp"
-#include <boost/simd/sdk/simd/pack.hpp>
-#include <boost/simd/sdk/simd/io.hpp>
-#include <boost/simd/include/functions/splat.hpp>
-#include <boost/simd/include/functions/plus.hpp>
-#include <boost/simd/include/functions/multiplies.hpp>
+#include <boost/simd/pack.hpp>
+#include <boost/simd/function/splat.hpp>
+#include <boost/simd/function/plus.hpp>
+#include <boost/simd/function/multiplies.hpp>
 #include <iostream>
 
 
 
 BOOST_AUTO_TEST_SUITE(boostSimdImport)
 
-BOOST_AUTO_TEST_CASE(SIMDFlagSupported)
-  {
-  std::cout << "******************SIMD CONFIG***********************"<< std::endl;
-  std::cout << "BOOST_SIMD_STRING " <<BOOST_SIMD_STRING<<std::endl;
-  std::cout << "BOOST_SIMD_STRING_LIST " << BOOST_SIMD_STRING_LIST << std::endl;
-  std::cout << "BOOST_SIMD_BYTES " << BOOST_SIMD_BYTES << std::endl;
-  std::cout << "BOOST_SIMD_BITS " << BOOST_SIMD_BITS << std::endl;
-  std::cout << "****************END SIMD CONFIG*********************" << std::endl;
-  }
+//BOOST_AUTO_TEST_CASE(SIMDFlagSupported)
+//  {
+//  std::cout << "******************SIMD CONFIG***********************"<< std::endl;
+//  std::cout << "BOOST_SIMD_STRING " <<BOOST_SIMD_STRING<<std::endl;
+//  std::cout << "BOOST_SIMD_STRING_LIST " << BOOST_SIMD_STRING_LIST << std::endl;
+//  std::cout << "BOOST_SIMD_BYTES " << BOOST_SIMD_BYTES << std::endl;
+//  std::cout << "BOOST_SIMD_BITS " << BOOST_SIMD_BITS << std::endl;
+//  std::cout << "****************END SIMD CONFIG*********************" << std::endl;
+//  }
 
 //http://nt2.metascale.fr/doc/html/tutorials/simd_hello_world.html
 BOOST_AUTO_TEST_CASE(tutorial)
